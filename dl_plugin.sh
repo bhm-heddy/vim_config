482561870f1ff58b1734a74afd190d2b61743ad9
Parrot Live user <user@localhost.localdomain>
2020-09-07 19:37:18 +0200

    script create bundle repository and download plugin

---
+++ dl_plugin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir ~/.vim/bundle
+
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/preservim/nerdcommenter.git ~/.vim/bundle/nerdcommenter
+git clone https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline.vim
+
