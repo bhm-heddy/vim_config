4f7a6aa1d966f69f8c0f83dd46003c30afd3138c
Heddy MERIEUX <hmerieux@e3r6p2.42.fr>
2021-06-23 15:14:45 +0200

    del stdheader , add ln

--- dl_plugin.sh
+++ dl_plugin.sh
@@ -6,3 +6,4 @@ git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/preservim/nerdcommenter.git ~/.vim/bundle/nerdcommenter
 git clone https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline.vim
 
+ln -s ~/.vim/vimrc ~/.vimrc
